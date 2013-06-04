973ba099b982dea74da560564527f7249dd89a8d
Ronaldo Barbachano <redcap3000@gmail.com>
2013-06-04 01:33:45 -0700

    code cleanup, removed unused server methods

--- server/server.js
+++ server/server.js
@@ -1,181 +1,88 @@
-if (Meteor.isServer) {
-  self= this;
+groups = new Meteor.Collection("groups");
 
-  
-    groups = new Meteor.Collection("groups");
+markers = new Meteor.Collection("markers");
 
-    markers = new Meteor.Collection("markers");
+marker_types = new Meteor.Collection("marker_types");
 
-    marker_types = new Meteor.Collection("marker_types");
+marker_services = new Meteor.Collection("marker_services");
 
-    
-    marker_services = new Meteor.Collection("marker_services");
+services = new Meteor.Collection("services");
 
-    services = new Meteor.Collection("services");
+Meteor.publish("allMarkers",function(){
+    return markers.find({},{});
+});
 
+Meteor.publish("allMarkerTypes",function(){
+    return marker_types.find({},{});
+});
 
-    
-    markers.allow({
-    
-        insert: function(userId,doc){
-            return (userId && doc.owner === userId);
-        },
-        update: function(userId,doc,fields,modifier){
-            return doc.owner === userId;
-        },
-        remove: function(userId,doc){
-            return doc.owner === userId;
-        },
-        fetch: ['owner']
-        
-    
-    });
-    Meteor.publish("allMarkers",function(){
-        return markers.find({},{});
-    });
-    
-    Meteor.publish("allMarkerTypes",function(){
-        return marker_types.find({},{});
-    });
-    
-    Meteor.publish("allGroups",function(){
-        return groups.find({},{});
-    });
+Meteor.publish("allGroups",function(){
+    return groups.find({},{});
+});
 
-    Meteor.publish("allMarkerServices",function(){
-        return marker_services.find({},{});
-    });
-    
-    Meteor.publish("allServices",function(){
-        return services.find({},{});
-    });
+Meteor.publish("allMarkerServices",function(){
+    return marker_services.find({},{});
+});
 
-    /*
-        marker_services = contains mongo id's refering to 'services'
-        
-        
-        _id : <mongo_id>,
-        marker_id : markers._id,
-        service_id : services._id
-     
-    */
- //   marker_services = new Meteor.Collection("marker_services");
-    
-    
+Meteor.publish("allServices",function(){
+    return services.find({},{});
+});
 
-//    agencies = new Meteor.Collection("agencies");
-
-   // services = new Meteor.Collection("services");
-
-  //markers._ensureIndex({ loc : "2d" });
-
-  //Org = new Meteor.Collection('Org');
-
-  //users = new Meteor.Collection("user");
-
-  Meteor.startup(function () {
-    // theres no org..
+Meteor.startup(function () {
+    // Pre fil the markertypes with some default types .. images are stored in /public
     if(marker_types.find({},{}).count() == 0){
-        console.log('filling marker types');
-        
+        console.log('filling marker types');        
         var default_markers = [
             {
                 name: 'Other',
                 owner : 'sys',
                 img : '/Other.png'
-                
             },
             {
                 name: 'Clinic',
                 owner : 'sys',
                 img : '/Clinic.png'
-                
             },
             {
                 name: 'Hospital',
                 owner : 'sys',
                 img : '/Hospital.png'
-                
             },
             {
                 name: 'Pharmacy',
                 owner : 'sys',
                 img : '/Pharmacy.png'
-                
             },
             {
                 name: 'Shelter',
                 owner : 'sys',
                 img : '/Shelter.png'
-                
             }
-
-
-
-        
         ];
         
-        for(var i=0;i<default_markers.length;i++){
+        for(var i=0;i<default_markers.length;i++)
             console.log(marker_types.insert(default_markers[i]));
-        }
         
     }
-
- });
+});
  
-   Meteor.methods({
-    isLoggedIn : function(uid){
-    
-    },
-    isAdmin : function(uid){
-        // do we have session here?
-        console.log(Session);
-        return true;
-    },
-    canEdit : function(curMarker){
-        if(curMarker){
+    // basic function that determines if the user can edit
+Meteor.methods({
+    canEdit : function(curMarker,theCollection){
+        if(curMarker && typeof theCollection == 'undefined'){
             var q = markers.findOne({_id: curMarker, owner: Meteor.userId()});
-            
-           if(q){
 
+        }else if(typeof theCollection != 'undefined'){
+            if(theCollection == 'groups')
+                // use a switch but for now support groups ?
+                var q = groups.findOne({_id:curMarker,owner:Meteor.userId()});
+                //else if(theCollection ==''){
+                //   var q = groups.findOne({_id:curMarker,owner:Meteor.userId()});
+        }
+        if(q){
             console.log(q);
-
             return q._id;
-            
-            }
         }
         return false;
-    },
-    findMarker : function(x,y){
-        console.log(x);
-        console.log(y);
-         markers._ensureIndex({ loc : "2d" });
-         var theCenter = [[x,y],.1];
-         // returns everything
-         var r = markers.find({ loc : { "$geoWithin" : { "$center" : theCenter }}}).fetch();
-         console.log(r.length);
-        return r;
-    },
-    findmarkers : function(){
-        console.log('finding markers');
-        var c = markers.find({},{}).fetch();
-        c.filter(function(arr){
-            console.log(arr);
-        });
-        return markers.find({},{}).fetch();
-    },
-    markersIndex : function(){
-        console.log('finding markers index');
-        var c = markers.find({},{}).fetch(),r=[];
-        c.filter(function(arr){
-            console.log(arr);
-            var x = {}
-            // use underscore for this..
-            x._id = arr._id;
-            x.marker_name = arr.marker_name;
-            r.push(x);
-        });
-        return r;
     }
-  });
-}
+});
