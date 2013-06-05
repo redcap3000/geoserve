e048073121d1e6d9eab8274daf76647c74bbc3e4
Ronaldo Barbachano <redcap3000@gmail.com>
2013-06-04 17:18:19 -0700

    code cleanup

--- client/lib/client.js
+++ client/lib/client.js
@@ -6,11 +6,10 @@
 Meteor.startup(function(){
     createMap();
     marker_sub = Meteor.subscribe("allMarkers");
-
     Deps.autorun(function(){
         if(marker_sub.ready()){
-            groups_sub = Meteor.subscribe("allGroups");
-            marker_types_sub = Meteor.subscribe("allMarkerTypes");
+            groups_sub = Meteor.subscribe("allGroups"),
+            marker_types_sub = Meteor.subscribe("allMarkerTypes"),
             services_sub = Meteor.subscribe("allServices");
 
             if(marker_types_sub.ready() && groups_sub.ready() && services_sub.ready()){
@@ -19,35 +18,34 @@ Meteor.startup(function(){
                 if(curMarker){
                     // load marker services and all services - but also need to trigger this when creating a new record...
                    if(typeof marker_services_sub == 'undefined'){
-                                                marker_services_sub = Meteor.subscribe("allMarkerServices");
+                        marker_services_sub = Meteor.subscribe("allMarkerServices");
                         var q = markers.findOne({_id:curMarker}).loc;
                         setMapCenter(q);
                         lookForMarkers();
-                    }else if(marker_services_sub.ready() && services_sub.ready()){
+                    }else if(marker_services_sub.ready()){
                         console.log('cur marker set inside of autorun');
                         var q = markers.findOne({_id: curMarker});
-                        if(q){
-                            if(typeof q['loc'] != 'undefined'){
+                        if(q)
+                            if(typeof q['loc'] != 'undefined')
                                 setMapCenter(q['loc']);
-                            }else{
+                            else{
                                 console.log('problem with mongo loc query');
                                 setMapCenter([0,0]);
                             }
-                        }
                         lookForMarkers();
                     }
                 // probably show something that allows us to edit the selected marker?
                 }else{
                     var mCheck = markers.find({},{}).fetch();
-                // maybe not 'create the map new each time?
+                    // maybe not 'create the map new each time?
                     if(typeof map === 'undefined' && !mCheck){
                         createMap();
                         setMapCenter([0,0]);
                     }else if (mCheck){
                         var emCheck = mCheck.pop();
-                        if(typeof emCheck != 'undefined'){
+                        if(typeof emCheck != 'undefined')
                             emCheck = emCheck['loc'];
-                        }else{
+                        else{
                             alert('It appears you are running geoserve for the first time!');
                             // ideally show the add marker screen
                             emCheck = [0,0];
@@ -65,14 +63,13 @@ Meteor.startup(function(){
 
 /*
  *   END Meteor.startup()
- * /
+ */
 
 /*
  *
  * Template Events
  *
  */
-
 
 // Deals with navigation showing/hiding elements; going to create screen/edit screen, and geolocation button.
 
@@ -110,22 +107,6 @@ Template.loggedInMenu.events({
     },
     'click .geolocate': function(evt,tmpl){
         if(navigator.geolocation){
-            var successFunction = function(success) {
-                  var navLatLng = new google.maps.LatLng(success.coords.latitude, success.coords.longitude);
-                  // annoying...
-                  //createMap(navLatLng);
-                  // send it true option to use different marker
-                  placeNavMarker(navLatLng,true);
-                  lookForMarkers([navLatLng.jb,navLatLng.kb]);
-                };
-
-            var errorFunction = function(success) {
-                var latlng = new google.maps.LatLng(37.808631, -122.474470);
-                //createMap(latlng);
-                placeNavMarker(navLatLng);
-            //  addAutocomplete();
-                };
-        
             navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
         }else{
             alert('Could not geolocate');
@@ -137,28 +118,17 @@ Template.loggedInMenu.events({
 
 Template.add_group.events({
     'click input.add_group': function(evt,tmpl){
-        var record ={};
+        var record ={},elements = tmpl.findAll(".group_visibility"),checked_element = undefined;
         record.owner = Meteor.userId();
         record.name = tmpl.find(".group_title").value;
         record.desc = tmpl.find(".group_desc").value;
-        //console.log(tmpl.find(".group_visibility"));
-        //record.visiblity = tmpl.find(".group_visibility").value;
         
-        
-        //  function getCheckedRadioId(name) {
-        var elements = tmpl.findAll(".group_visibility");
         // properly determines the checked element for similarlly classed values
-        var checked_element = undefined;
         for (var i=0, len=elements.length; i<len; ++i)
             if (elements[i].checked) var checked_element =  elements[i].value;
         
-        console.log('checked element:');
-        console.log(checked_element);
-        if(typeof checked_element !== 'undefined')
-            record.visibility = checked_element;
-        else{
-            record.visibility = 'private';
-        }
+        record.visibility = (typeof checked_element !== 'undefined'? checked_element: 'private');
+        
         var record_id = groups.insert(record);
         Session.set('selected_group',record_id);
 
@@ -234,11 +204,7 @@ Template.add_marker.events({
 // Handles edit /deleting markers
 // TODO Remove all associated services when removing a marker, create server side methods for secure deletion etc.
 
-
-
-
 Template.groups.events({
-
     'click .edit_group': function(evt,tmpl){
         console.log('edit group');
         // weird bug.. click was only getting the first classed element...
@@ -258,15 +224,10 @@ Template.groups.events({
 }
 );
 
-
 Template.markers.events({
     'click input.del_marker' : function(evt,tmpl){
         markers.remove({_id:tmpl.data._id});
         $('#marker_add'),show();
-        // refresh the overlays
-        // eventually use SESSION variable to keep track of how to filter the markers
-        // based on the user settings/permissions etc.
-//        lookForMarkers();
     },
     'click a.edit_marker' :function(evt,tmpl){
         /* set session and show marker editor if admin*/
@@ -279,7 +240,6 @@ Template.markers.events({
             Session.set('selected_marker',tmpl.data._id);
         }
         $('div#groups').hide();
-
     }
 });
 
@@ -312,16 +272,13 @@ Template.services_offered.events({
         console.log(record);
     },
     'click input.add_service' : function(evt,tmpl){
-    
         console.log('adding this service');
-    
         var new_service = tmpl.find('.add_services'),
-            record = {};
+            record = {},
             marker_id = Session.get('selected_marker');
         if(marker_id){
             record.service = new_service.options[new_service.selectedIndex].id;
             record.marker_id = marker_id;
-            //alert('inserting');
             // make sure no DUPES!!
             marker_services.insert(record);
             // owner? possibly agency? not super needed...
@@ -334,36 +291,24 @@ Template.services_offered.events({
     }
 });
 
-
-/* 
+/*
  *
  *
  *   Template Functions
  *
  */
 
-
-
 Template.groups.selectedGroup = function(evt,tmpl){
     var group_id = Session.get('selected_group');
     console.log('in selected group');
     if(Meteor.userId() && group_id){
-        var q =groups.findOne({_id: group_id });
-        console.log(q);
-//        q = q.fetch();
-  //      console.log(q);
-        
-        var q2=group_codes.find({owner:Meteor.userId(),group_id:group_id});
-        
+        var q =groups.findOne({_id: group_id }), q2=group_codes.find({owner:Meteor.userId(),group_id:group_id});
         q2 = q2.fetch();
-        
-        if(q2.length > 0){
+        if(q2.length > 0)
             // return the codes so template renders them ...
             console.log(q2);
-        }else{
+        else
             console.log('no codes');
-        }
-        
         return q;
     }
 };
@@ -372,21 +317,11 @@ Template.groups.selected_visibility = function(evt,tmpl){
     // check to see if element is selected or not ?
     var group_id = Session.get('selected_group');
     if(Meteor.userId() && group_id){
-        console.log('in groups visibility');
-        var q = groups.findOne({_id: group_id},{visibility:1});
-        
-        console.log(q.visibility);
         // just return entire html block with the select menu and the appropriate value selected SUCH A PITA!!!
-        var vis = ['public','private','invite'];
-        
-        var result = '';
-        
-        for(var n = 0; n< vis.length;n++){
+        for(var n = 0,vis = ['public','private','invite'],result='',q=groups.findOne({_id: group_id},{visibility:1}); n< vis.length;n++)
             result += '<input type="radio" name="group_visbility" class="span group_visibility" id="gv_'+vis[n]+'" value="'+vis[n]+'" '+(vis[n] == q.visibility ? ' CHECKED ':'' )+'  ><label>'+vis[n]+'</label>';
 
-        }
-      
-        console.log(result);
+        
         return result;
     }
 }
@@ -395,21 +330,12 @@ Template.editor_group.selectedGroup = Template.groups.selectedGroup;
 
 Template.edit_marker.canEdit = function(evt,tmpl){
     var user_id = Meteor.userId(), marker_id = Session.get('selected_marker'), can_edit = Session.get('can_edit');
-    
     // kinda insecure... probably destroy session if user_id isn't around ...
-    if(can_edit == marker_id && user_id){
+    if(can_edit == marker_id && user_id)
         return true;
-    }else{
-        if(marker_id && user_id)
-        
-            Meteor.call('canEdit',marker_id,function(error,result){
-                if(result)
-                    Session.set('can_edit',result);
-            
-            });
-        return false;
-    }
-
+    else if(marker_id && user_id)
+        Meteor.call('canEdit',marker_id,function(error,result){ if(result) Session.set('can_edit',result); });
+    return false;
 }
 
 Template.group_menu.userGroups = function(evt,tmpl){
@@ -418,12 +344,7 @@ Template.group_menu.userGroups = function(evt,tmpl){
     return q;
 };
 
-
-//Template.group_invites.
-
-
 Template.add_marker.userGroups = Template.group_menu.userGroups;
-
 
 Template.add_marker.markerTypes = function(evt,tmpl){
     var q = marker_types.find({},{});
@@ -432,22 +353,13 @@ Template.add_marker.markerTypes = function(evt,tmpl){
 };
 
 Template.services_offered.services = function(evt,tmpl){
-
-
-    if(typeof services_sub != 'undefined'){
-        console.log(services_sub);
-    
+    if(typeof services_sub != 'undefined')
         if(services_sub.ready()){
             var q = services.find({},{});
             q = q.fetch();
-            console.log('looking up services in services_offered > services');
-            console.log(q);
             return q;
-        }else{
+        }else
             console.log('services sub not ready');
-//            Template.services_offered.services;
-        }
-    }
 };
 
 Template.edit_marker.currentServices = function(evt,tmpl){
@@ -461,12 +373,11 @@ Template.edit_marker.currentServices = function(evt,tmpl){
                 var q = services.findOne({_id: currentServ[i].service});
                 if(typeof q.title != 'undefined')
                     currentServ[i].title = q.title;
-                
             }
             return currentServ;
-            }
+        }
     }
-}
+};
 
 /* 
  *
