d7326eb6b040366cc427270302f82c52e7a9240c
Ronaldo Barbachano <redcap3000@gmail.com>
2013-06-04 18:50:54 -0700

    fixed services inside of selected marker

--- client/lib/client.js
+++ client/lib/client.js
@@ -11,29 +11,24 @@ Meteor.startup(function(){
             groups_sub = Meteor.subscribe("allGroups"),
             marker_types_sub = Meteor.subscribe("allMarkerTypes"),
             services_sub = Meteor.subscribe("allServices");
-
-            if(marker_types_sub.ready() && groups_sub.ready() && services_sub.ready()){
+            marker_services_sub = Meteor.subscribe("allMarkerServices");
+            if(marker_types_sub.ready() && groups_sub.ready() && services_sub.ready() && marker_services_sub.ready()){
                 // this sets the new loc prematurely?
                 var curMarker = Session.get('selected_marker');
                 if(curMarker){
                     // load marker services and all services - but also need to trigger this when creating a new record...
-                   if(typeof marker_services_sub == 'undefined'){
-                        marker_services_sub = Meteor.subscribe("allMarkerServices");
-                        var q = markers.findOne({_id:curMarker}).loc;
-                        setMapCenter(q);
-                        lookForMarkers();
-                    }else if(marker_services_sub.ready()){
-                        console.log('cur marker set inside of autorun');
-                        var q = markers.findOne({_id: curMarker});
-                        if(q)
-                            if(typeof q['loc'] != 'undefined')
-                                setMapCenter(q['loc']);
-                            else{
-                                console.log('problem with mongo loc query');
-                                setMapCenter([0,0]);
-                            }
-                        lookForMarkers();
-                    }
+                   
+                    console.log('cur marker set inside of autorun');
+                    var q = markers.findOne({_id: curMarker});
+                    if(q)
+                        if(typeof q['loc'] != 'undefined')
+                            setMapCenter(q['loc']);
+                        else{
+                            console.log('problem with mongo loc query');
+                            setMapCenter([0,0]);
+                        }
+                    lookForMarkers();
+                
                 // probably show something that allows us to edit the selected marker?
                 }else{
                     var mCheck = markers.find({},{}).fetch();
