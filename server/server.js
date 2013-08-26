f3ae91e6269c5907057637e4124f70b5cc4c8cd3
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2013-08-26 16:39:46 -0700

    instageo now updates all available existing posts according to instagrams users feed limit

--- server/server.js
+++ server/server.js
@@ -6,7 +6,53 @@ Meteor.publish("usersGroupCodes",function(){
     return users_group_codes.find({owner:Meteor.userId()},{});
 });
 
+//
 
+
+
+// should allow to filter nearly any response from instaGram and store it ...? hope scopes are ok for this !
+instaFilter =
+    function(arr){
+        if(arr.location != null){
+    // this is checking if insta_grams is present client/side so it gets reinserted! wait for the insta_grams collection to be ready?
+            var existing_check = insta_grams.findOne({id:arr.id});
+        if(existing_check){
+            if(arr.likes != null && existing_check.likes != arr.likes.count){
+                insta_grams.update(existing_check._id,{"$set" :{ likes : arr.likes.count}});
+            }
+        }else{
+
+            var r = {
+                id : arr.id,
+                username : arr.user.username,
+                link : arr.link,
+                created_time : arr.created_time,
+                image_low : arr.images.low_resolution.url,
+                image_standard : arr.images.standard_resolution.url,
+                image_thumb : arr.images.thumbnail.url,
+                type : arr.type
+            };
+            
+            if(arr.caption != null){
+                r.caption = arr.caption.text,
+                r.caption_id = arr.caption.id;
+            }
+            
+            if(arr.tags != null){
+                r.tags = arr.tags;
+            }
+            
+            if(arr.likes != null){
+                 r.likes = arr.likes.count;
+            }
+            r.lat = arr.location.latitude;
+            r.lon = arr.location.longitude;
+            r.owner = Meteor.userId();
+            console.log(insta_grams.insert(r));
+        }
+        }
+
+    };
 
 
 // basic function that determines if the user can edit
@@ -75,53 +121,23 @@ Meteor.methods({
                     if(typeof request.data.data != 'undefined'){
                         // filter data
                         var result = [];
-               
-               
-               
-               
-
-                        request.data.data.filter(function(arr){
-                            if(arr.location != null){
-                        // this is checking if insta_grams is present client/side so it gets reinserted! wait for the insta_grams collection to be ready?
-                                var existing_check = insta_grams.findOne({id:arr.id});
-                            if(existing_check){
-                                if(arr.likes != null && existing_check.likes != arr.likes.count){
-                                    insta_grams.update(existing_check._id,{"$set" :{ likes : arr.likes.count}});
+                        if(typeof request.data.pagination.next_url != 'undefined')
+                        Meteor.call('user_self_backlog',request.data.pagination.next_url,
+                            function(error,result){
+                                if (result){
+                                    //console.log(result.pagination);
+                                    console.log('success');
+                                    if(typeof result.pagination != 'undefined'){
+                                    // wait a bit to not overwhelm server...
+                                        console.log('recursing');
+                                        var pagination_timeout = Meteor.setTimeout(Meteor.call('user_self_backlog',result.pagination.next_url),60*60*1);
+                                    }
                                 }
-                            }else{
-
-                                var r = {
-                                    id : arr.id,
-                                    username : arr.user.username,
-                                    link : arr.link,
-                                    created_time : arr.created_time,
-                                    image_low : arr.images.low_resolution.url,
-                                    image_standard : arr.images.standard_resolution.url,
-                                    image_thumb : arr.images.thumbnail.url,
-                                    type : arr.type
-                                };
-                                
-                                if(arr.caption != null){
-                                    r.caption = arr.caption.text,
-                                    r.caption_id = arr.caption.id;
-                                }
-                                
-                                if(arr.tags != null){
-                                    r.tags = arr.tags;
-                                }
-                                
-                                if(arr.likes != null){
-                                     r.likes = arr.likes.count;
-                                }
-                                r.lat = arr.location.latitude;
-                                r.lon = arr.location.longitude;
-                                r.owner = Meteor.userId();
-                                result.push(r);
-                                insta_grams.insert(r);
+                                else if(typeof error != undefined)
+                                    console.log(error);
                             }
-                            }
-               
-                        });
+                        );
+                        request.data.data.filter(instaFilter);
 //                        return request.data.data;
                             return true;
                
@@ -139,8 +155,40 @@ Meteor.methods({
             else
                 return {error:'Access token required for user_self'};
             
-        }
-    
+        },
+     user_self_backlog : function(url){
+         //   if(typeof access_token != 'undefined'){
+                this.unblock();
+                var request = HTTP.get(url);
+                if(request.statusCode === 200 && typeof request.data != 'undefined'){
+                    if(typeof request.data.data != 'undefined'){
+                        // filter data
+                        var result = [];
+                        if(typeof request.data.pagination.next_url != 'undefined'){
+                                    // wait a bit to not overwhelm server...
+                            Meteor.call('user_self_backlog',request.data.pagination.next_url);
+                        }
+               
+               
+               
+                        request.data.data.filter(instaFilter);
+                        return true;
+               
+                    }else{
+                        return request.data;
+                    }
+               // set the interval if not already set ? 
+                   }
+               else{
+                    console.log('problem with request');
+                    console.log(request);
+               
+               }
+          //     }
+          //  else
+          //      return {error:'Access token required for user_self'};
+            
+        }   
     }
    );
 
