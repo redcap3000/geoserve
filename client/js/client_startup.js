f23c383f3e88af9106f931ccb97aa813fe8e6fa1
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2013-08-26 14:09:10 -0700

    reverted client startup changes ..

--- client/js/client_startup.js
+++ client/js/client_startup.js
@@ -9,17 +9,13 @@ Meteor.startup(function(){
     Deps.autorun(function(){
         if(Meteor.userId()){
             // also use this reactive source to determine interface elements in templates...
-            //if(typeof instaGramPosts == 'undefined')
-                instaGramPosts = Meteor.subscribe("userInstaGrams");
-            var doesHaveAccess = Session.get('access_token');
+            instaGramPosts = Meteor.subscribe("userInstaGrams");
 
-            if( !doesHaveAccess){
-               if(access_token.length > 1 ){
-                    var access_token = window.location.href.split("#");
-
-                    access_token = access_token[1].split("=")[1];
-                    Session.set('access_token', access_token);
-                 }
+            var access_token = window.location.href.split("#");
+            if(access_token.length > 1 && !doesHaveAccess){
+                var doesHaveAccess = Session.get('access_token');
+                access_token = access_token[1].split("=")[1];
+                Session.set('access_token', access_token);
             }
             
             var instaGram = Session.get('user_self');
