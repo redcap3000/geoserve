30279e0332ac68a419347746d4133d4fbce150b3
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2013-08-29 09:56:16 -0700

    added basic client-side collection filters, likes, last update,date, and default - mix of date and likes

--- client/js/client.js
+++ client/js/client.js
@@ -1,23 +1,12 @@
 // for hiding the loginto instagram button ..
 
-Template.nav.instaPostReady = function(){
+
+Template.nav.instaPostReady =function(){
     return !Session.equals("user_self",false);
 };
-Template.nav.hasInstaCode = function(){
-    return (Session.get('access_token') ? true : false);
+Template.nav.hasInstaCode =  function(){
+    return (Session.get("access_token")?true:false);
 };
-
-Meteor.startup(
-    function(){
-
-        if(Meteor.userId()){
-            Meteor.setInterval(function(){
-                Session.set('user_self',false);
-                }        ,60 * 60 * 30);
-        }
-                   
-    }
-);
 
 
 Template.nav.events = {
@@ -32,53 +21,59 @@ Template.nav.events = {
                             // redirect here...
                            window.location.replace(result);
                         });
-                        }else if(doesHaveAccess){
-                            Meteor.call('request_auth_code',doesHaveAccess,Meteor.settings.redirect_uri,
-                                function(error,result){
-                                    if(typeof error =='undefined'){
-                                        Session.set('access_token',result.access_token);
-                                        Session.set('user_info',result.user);
-                                        // do default call for user feed ... to populate map with markers...
-                                        // set the interval to continually fetch new results ??
-                                        
-                                        
-                                        Meteor.call('user_self',result.access_token,function(error,result){
-                                                if(typeof error =='undefined'){
-                                                    Session.set('user_self',result);
-                                                }else{
-                                                   // alert('error');
-                                                    console.log(error);
-                                            }}
-                                        );
-                                        
-                                        
-                                    }else
-                                        console.log(error);
-                                }
-                            );
-                        }
-            },
+            }else if(doesHaveAccess){
+                Meteor.call('request_auth_code',doesHaveAccess,Meteor.settings.redirect_uri,
+                    function(error,result){
+                        if(typeof error =='undefined'){
+                            Session.set('access_token',result.access_token);
+                            Session.set('user_info',result.user);
+                            Session.set('user_self',false);
+                            // do default call for user feed ... to populate map with markers...
+                            // set the interval to continually fetch new results ??
+                            
+                        }else
+                            console.log(error);
+                    }
+                );
+            }
+        },
     'click .user_self' : function(){
         var access_token = Session.get('access_token');
-        //var user_feed = Session.get('user_self');
-        // only run this if 
-         if(access_token){
-            Session.set('user_self',false);
-             Meteor.call('user_self',access_token,function(error,result){
-                                if(typeof error =='undefined'){
-                                    Session.set('user_self',result);
-                                }else{
-                                   // alert('error');
-                                    console.log(error);
-                            }}
-                            );
+         if(access_token && Session.get('user_self')){
+             Session.set('user_self',false);
         }
+    },
+    'click #mFilterLikes': function(evt,tmpl){
+        Session.set('markerSort', {likes:-1});
+    },
+    'click #mFilterLastChange': function(evt,tmpl){
+        console.log('last hit');
+        Session.set('markerSort', {lastHit:1});
+    },
+    'click #mFilterDate': function(evt,tmpl){
+        Session.set('markerSort', {id:-1});
+    },
+    'click #mFilterDefault': function(evt,tmpl){
+        Session.set('markerSort', undefined);
     }
+    
 };
 
+/*
+ *  these are the 'main' markers ... do some stuff with session and sorting next ?
+ *
+ */
+
 Template.loggedInMenu.instaMarkers = function(){
-// check if marker sub is ready?
-    return insta_grams.find({},{sort:{id: -1}});
+    // default filter ...
+    var filter = {};
+    if(!Session.equals('markerSort',undefined)){
+        // does this work?
+        filter.sort = Session.get('markerSort');
+    }else{
+        filter.sort = {id:-1,lastHit:1,likes:-1};
+    }
+    return insta_grams.find({},filter);
 }
 
 Template.instaMarker.events = {
@@ -86,9 +81,12 @@ Template.instaMarker.events = {
         setMapCenter([this.lat,this.lon]);
     }
 }
-
+/*
+ *  Place nav marker and setup google marker with a basic thing of its text .. only run this once? Need to figure
+ *  out how to update this with the latest data .... could do a check on 'rendered' to see if it has changed or not from
+ *  previous values ?
+ */
 Template.instaMarker.created = function(){
-//console.log(this.data);
        placeNavMarker(new google.maps.LatLng(this.data.lat,this.data.lon),this.data.image_thumb,this.data.likes + ' likes' + (this.data.tags.length > 0  ? '\n' + this.data.tags.join(', ')  :'') );
 }
 
